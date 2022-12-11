a8431cee281c0747a565c1a4679caa2b018c4a4d
Diran sai <dirantechie@gmail.com>
2022-12-11 10:56:59 +0200

    update styles

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const productsContainer = document.querySelector(".products");
 const increasingBtn = document.querySelector(".increase-btn");
 const decreasingBtn = document.querySelector(".decrease-btn");
 
+const headerBottomMenu = document.querySelector(".header-bottom");
+
 let productsPos = 0;
 
 function handleProducts(e, status) {
@@ -19,3 +21,15 @@ function handleProducts(e, status) {
 
 increasingBtn.addEventListener("click", (e) => handleProducts(e, true));
 decreasingBtn.addEventListener("click", (e) => handleProducts(e, false));
+
+
+
+document.addEventListener("scroll", (e) => {
+    const scrollTop = window.scrollY;
+
+    if (scrollTop >= 200) {
+        headerBottomMenu.classList.add("header-bottom-sticky");
+    } else {
+        headerBottomMenu.classList.remove("header-bottom-sticky");
+    }
+});
